e0403068c80610c01749d61d077431d283216aff
thilan1998 <80124129+thilan1998@users.noreply.github.com>
2021-08-24 17:00:59 +0530

    Add files via upload

---
+++ IWT/src/js/myStyles.js
@@ -0,0 +1,16 @@
+function loadData(name) {
+
+	if (name=="button1") {
+
+	document.getElementById("img4").src="images/img4.jpg";
+	document.getElementById("para").innerHTML="sample paragraph sample paragraph sample paragraph sample paragraph sample paragraph sample paragraph sample paragraph sample ";
+					}
+	else if(name=="button2"){
+		document.getElementById("img5").src="images/img5.jpg";
+		document.getElementById("para").innerHTML="sample paragraph 222222222222222 sample paragraph sample paragraph sample paragraph sample paragraph sample paragraph sample paragraph sample ";
+	}
+	else if(name=="button3"){
+		document.getElementById("img6").src="images/img6.jpg";
+		document.getElementById("para").innerHTML="sample paragraph sample paragraph 333333333333333333 sample paragraph sample paragraph sample paragraph sample paragraph sample paragraph sample ";
+	}
+}
